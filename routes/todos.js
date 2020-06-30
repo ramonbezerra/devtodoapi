898687b3a74837296022123174f1aedb86ba4bdc
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-06-30 16:44:43 -0300

    POST, PUT and DELETE todo's methods

--- routes/todos.js
+++ routes/todos.js
@@ -16,4 +16,61 @@ router.get('/', function(req, res, next) {
     }));
 });
 
+/* POST todo */
+router.post('/', function(req, res, next) {
+    const {name} = req.body;
+    model.Todo.create({
+        name: name,
+        done: false
+    })
+    .then(todo => res.status(201).json({
+        error: false,
+        data: todo,
+        message: 'New todo has been created'
+    }))
+    .catch(error => res.json({
+        error: true,
+        data: [],
+        message: error
+    }));
+});
+
+router.put('/:id', function(req, res, next) {
+    const id = req.params.id;
+    const {name, done} = req.body;
+    model.Todo.update({
+        name: name,
+        done: done
+    },{ 
+        where: {
+            id: id
+        }
+    })
+    .then(todo => res.status(201).json({
+        error: false,
+        message: 'todo has been updated'
+    }))
+    .catch(error => res.json({
+        error: true,
+        message: error
+    }));
+});
+
+router.delete('/:id', function(req, res, next) {
+    const id = req.params.id;
+    model.Todo.destroy({
+        where: {
+            id: id
+        }
+    })
+    .then(todo => res.status(201).json({
+        error: false,
+        message: 'todo has been deleted'
+    }))
+    .catch(error => res.json({
+        error: true,
+        message: error
+    }));
+});
+
 module.exports = router;
