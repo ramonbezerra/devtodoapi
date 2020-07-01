74f36e0300f09be4e65756f693bc624e73354ace
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-06-30 22:19:21 -0300

    GET todos listing by status method

--- routes/todos.js
+++ routes/todos.js
@@ -2,9 +2,27 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
-/* GET todos listing. */
+/* GET todos listing */
 router.get('/', function(req, res, next) {
   model.Todo.findAll({})
+    .then(todos => res.json({
+        error: false,
+        data: todos
+    }))
+    .catch(error => res.json({
+        error: true,
+        data: [],
+        message: error
+    }));
+});
+
+/* GET todos listing by status */
+router.get('/:done', function(req, res, next) {
+    model.Todo.findAll({
+        where: {
+            done: req.params.done == 'true' ? true : false
+        }
+    })
     .then(todos => res.json({
         error: false,
         data: todos
